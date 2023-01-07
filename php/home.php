9acea396f75b64dc005298d0cc98c2848f22d8e9
Lewis ushindi <coderflame3@gmail.com>
2023-01-07 10:17:22 +0300

    changing bg color

--- php/home.php
+++ php/home.php
@@ -50,8 +50,9 @@ $sid=$_SESSION['id'];
 </head>
 <body onLoad="document.showexp.edetail.focus()">
 
-<section>
-    <h4 class="h-12 flex justify-between align-middle items-center mb-2 px-2 bg-blue-600 text-white">Welcome <?php echo $_SESSION['uname']; ?> <a href='signout.php' class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" >Logout</a></h4> 
+<section class="bg-blue-300">
+    <h4 class="h-12 flex justify-between align-middle items-center mb-2 px-2 bg-blue-600 text-white">Welcome <?php 
+    echo $_SESSION['uname']; ?> <a href='signout.php' class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" >Logout</a></h4> 
     Total Earning : <span class="label label-success">
     <?php 
     $today = date("Y-m-d");
