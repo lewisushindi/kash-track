298f5fd7745231d6b4e4e24888d4d36d6f6fe655
Lewis ushindi <coderflame3@gmail.com>
2023-01-18 20:25:19 +0300

    changing to fixed position

--- php/analytics.php
+++ php/analytics.php
@@ -49,7 +49,7 @@ $sid=$_SESSION['id'];
 </head>
 <body onLoad="document.showexp.edetail.focus()">
     <section>
-        <div class="h-12 flex justify-between align-middle items-center px-2 bg-blue-600 text-white">
+        <div class="h-12 flex justify-between align-middle items-center px-2 bg-blue-600 text-white fixed top-0 left-0 right-0">
             <div class="flex justify-center items-center align-middle">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
