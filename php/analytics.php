fd36eeb2c28671a5c50d50796602a7e648000791
Lewis ushindi <coderflame3@gmail.com>
2023-01-18 17:54:45 +0300

    displaying user name & logout btn

--- php/analytics.php
+++ php/analytics.php
@@ -49,6 +49,19 @@ $sid=$_SESSION['id'];
 </head>
 <body onLoad="document.showexp.edetail.focus()">
     <section>
+        <div class="h-12 flex justify-between align-middle items-center px-2 bg-blue-600 text-white">
+            <div class="flex justify-center items-center align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+                <?php echo $_SESSION['uname']; ?> 
+            </div>            
+            <a href='signout.php' class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" >Logout</a>
+        </div>
+
+
+
+
         <div class="flex justify-between absolute left-0 bottom-0 right-0 items-center h-11 text-white bg-blue-600 align-middle px-4">
             <div class="flex flex-col justify-center align-middle items-center" >
                 <div class="img mb-0">
