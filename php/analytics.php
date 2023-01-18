e5c0b878b34f038bc970121f2aa940c865357db4
Lewis ushindi <coderflame3@gmail.com>
2023-01-18 20:21:55 +0300

    adding expense report from database

--- php/analytics.php
+++ php/analytics.php
@@ -58,8 +58,90 @@ $sid=$_SESSION['id'];
             </div>            
             <a href='signout.php' class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" >Logout</a>
         </div>
+        <div class="h-16 flex justify-between items-center w-full px-2 text-white bg-blue-400">
+            <div class="flex items-center justify-center align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
+                    <path d="M1 1.75A.75.75 0 011.75 1h1.628a1.75 1.75 0 011.734 1.51L5.18 3a65.25 65.25 0 0113.36 1.412.75.75 0 01.58.875 48.645 48.645 0 01-1.618 6.2.75.75 0 01-.712.513H6a2.503 2.503 0 00-2.292 1.5H17.25a.75.75 0 010 1.5H2.76a.75.75 0 01-.748-.807 4.002 4.002 0 012.716-3.486L3.626 2.716a.25.25 0 00-.248-.216H1.75A.75.75 0 011 1.75zM6 17.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15.5 19a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+                </svg>
+                <a href="expense.php">Expenses</a>
+            </div>
+            <div class="flex items-center justify-center align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
+                    <path fill-rule="evenodd" d="M1 4a1 1 0 011-1h16a1 1 0 011 1v8a1 1 0 01-1 1H2a1 1 0 01-1-1V4zm12 4a3 3 0 11-6 0 3 3 0 016 0zM4 9a1 1 0 100-2 1 1 0 000 2zm13-1a1 1 0 11-2 0 1 1 0 012 0zM1.75 14.5a.75.75 0 000 1.5c4.417 0 8.693.603 12.749 1.73 1.111.309 2.251-.512 2.251-1.696v-.784a.75.75 0 00-1.5 0v.784a.272.272 0 01-.35.25A49.043 49.043 0 001.75 14.5z" clip-rule="evenodd" />
+                </svg>
+                <a href="#">Income</a>
+            </div>
+            <div class="flex items-center justify-center align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
+                    <path fill-rule="evenodd" d="M9.674 2.075a.75.75 0 01.652 0l7.25 3.5A.75.75 0 0117 6.957V16.5h.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H3V6.957a.75.75 0 01-.576-1.382l7.25-3.5zM11 6a1 1 0 11-2 0 1 1 0 012 0zM7.5 9.75a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5zm3.25 0a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5zm3.25 0a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5z" clip-rule="evenodd" />
+                </svg>
+                <a href="#">Trends</a>
+            </div>
+
+        </div>
 
 
+        <?php
+            if (!empty($_POST["endd"])) {
+            $dstart = $_POST['startd'];
+            $dend = $_POST['endd'];
+
+            $dstart = strtotime($dstart);
+            $dend = strtotime($dend);
+
+            $dstart= date('Y-m-d', $dstart);
+            $dend = date('Y-m-d', $dend);
+            }
+            else
+            {
+                $dstart = date("Y-m-01");
+                $dend = date("Y-m-d");
+            }
+            $expdetail = '';
+            if(!empty($_POST['expdetail']))
+            {
+            $expdetail = mysqli_real_escape_string($conn, $_POST['expdetail']);
+            }
+            $dstartn = strtotime($dstart);
+            $dstartn = date('d M Y', $dstartn);
+            $dendn = strtotime($dend);
+            $dendn = date('d M Y', $dendn);
+        ?>
+
+        <div class="flex flex-col text-sm p-2 bg-blue-100 h-full min-h-screen">
+            <section>
+                <h4 class="mb-2">
+                    Expense Report from <?php echo $dstartn; ?> - <?php echo $dendn; ?>
+                </h4>
+            </section>
+          
+            <div class="flex justify-between items-center">
+                <div class="flex flex-col justify-center items-center w-full h-12 bg-blue-200" >Date</div>
+                <div class="flex flex-col justify-center items-center w-full h-12 bg-blue-300">Expense</div>      
+                <div class="flex flex-col justify-center items-center w-full h-12 bg-blue-400">amount</div>
+            </div>
+            <?php 
+
+            $sql = "SELECT * FROM expense WHERE date >= '$dstart' AND date <= '$dend' AND uid='$sid' AND pname LIKE '%$expdetail%' AND isdel=0 ORDER by date ";
+
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) 
+                {
+                while($row = $result->fetch_assoc()) 
+                {
+                $exdate = strtotime($row["date"]);
+                $exdate = date('d M Y', $exdate);
+                    echo "<div class='flex justify-between items-center w-full'> <div class='flex flex-col justify-center items-center w-full bg-blue-400 h-14 border-b-2 border-solid border-b-white'> " . $exdate. "</div> <div class='flex justify-center flex-col items-center w-full bg-blue-200 h-14 border-b-2 border-solid border-b-white'> " . $row["pname"]. " </div> <div class='flex flex-col justify-center align-middle items-center bg-blue-300 w-full h-14 border-b-2 border-solid border-b-white'> Kshs ". $row["pprice"]. "</div> </div>";
+                }
+            } else {
+                    echo "<div class='flex justify-center items-center '> <div> </div> <div class='alert alert-danger' role='alert'> No Expense in given Dates </div><div> </div></div>";
+            }
+     
+
+            ?>     
+            
+
+        </div>
 
 
         <div class="flex justify-between absolute left-0 bottom-0 right-0 items-center h-11 text-white bg-blue-600 align-middle px-4">
