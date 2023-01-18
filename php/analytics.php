3d5f17fc537ebe3a3090e2b24987707ee991b684
Lewis ushindi <coderflame3@gmail.com>
2023-01-18 20:39:38 +0300

    style adjustments

--- php/analytics.php
+++ php/analytics.php
@@ -59,7 +59,7 @@ $sid=$_SESSION['id'];
             <a href='signout.php' class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" >Logout</a>
         </div>
         <div class="h-16 fixed top-0 left-0 right-0 mt-12 flex justify-between items-center w-full px-2 text-white bg-blue-400">
-            <div class="flex items-center justify-center align-middle">
+            <div class="flex items-center justify-center align-middle bg-gray-100 text-blue-500 px-1 py-2 rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
                     <path d="M1 1.75A.75.75 0 011.75 1h1.628a1.75 1.75 0 011.734 1.51L5.18 3a65.25 65.25 0 0113.36 1.412.75.75 0 01.58.875 48.645 48.645 0 01-1.618 6.2.75.75 0 01-.712.513H6a2.503 2.503 0 00-2.292 1.5H17.25a.75.75 0 010 1.5H2.76a.75.75 0 01-.748-.807 4.002 4.002 0 012.716-3.486L3.626 2.716a.25.25 0 00-.248-.216H1.75A.75.75 0 011 1.75zM6 17.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15.5 19a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                 </svg>
